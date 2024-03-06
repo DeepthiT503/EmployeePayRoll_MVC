8eba34e939e01a235c98cca40afb91a3ae2385c7
Deepthi Thalari <204g5a0503@srit.ac.in>
2024-03-06 08:00:00 +0530

    added

---
+++ EmployeePayRoll/Controllers/AjaxController.cs
@@ -0,0 +1,27 @@
+﻿using BusineessLayer.Interface;
+using Microsoft.AspNetCore.Mvc;
+using RepositoryLayer.Entity;
+
+namespace EmployeePayRoll.Controllers
+{
+    public class AjaxController : Controller
+    {
+        private readonly IEmployeeBussiness employeeBussiness;
+        public AjaxController(IEmployeeBussiness employeeBussiness)
+        {
+            this.employeeBussiness = employeeBussiness;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet("Getallemp")]
+        public IActionResult GetAllEmployeeByAjax()
+        {
+            List<EmployeeEntity> employee = employeeBussiness.GetAllEmployees().ToList();
+            return new JsonResult(employee);
+        }
+
+    }
+}
